3ab1122f72a6f62f8551f75d9b63ce3995346a79
Yuval Dinari <ydinari@vonage.com>
2015-11-02 15:21:38 +0200

    Remove some files

--- mserver/SIPRequest.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  SIPRequest.cpp
-//  mserver
-//
-//  Created by Yuval Dinari on 10/27/15.
-//  Copyright © 2015 Vonage. All rights reserved.
-//
-
-#include "SIPRequest.hpp"
--- mserver/SIPResponse.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  SIPResponse.cpp
-//  mserver
-//
-//  Created by Yuval Dinari on 10/27/15.
-//  Copyright © 2015 Vonage. All rights reserved.
-//
-
-#include "SIPResponse.hpp"
